fe7109fad2d56a1141a92ba0dbd16fba23ac053e
hakjun <hiatus9014@gmail.com>
2026-03-24 17:29:55 +0900

    3.24 poly ex

--- src/poly/ex/pay0/PayService.java
+++ src/poly/ex/pay0/PayService.java
@@ -1,11 +1,12 @@
 package poly.ex.pay0;
 
-public class PayService {
-
+public class PayService {//개방,폐쇄(새로운결제수단이 추가되도 코드가 변경되지않아야함)
+//Strategy Pattern도입해야한다 -> 바뀌는 부분을 따로 인터페이스로 캡슐화
     public void processPay(String option, int amount) {
+
         boolean result;
-        System.out.println("결제를 시작합니다: option=" + option + ", amount=" +
-                amount);
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
+
         if (option.equals("kakao")) {
             KakaoPay kakaoPay = new KakaoPay();
             result = kakaoPay.pay(amount);
@@ -13,13 +14,14 @@ public class PayService {
             NaverPay naverPay = new NaverPay();
             result = naverPay.pay(amount);
         } else {
-            System.out.println("결제 수단이 없습니다.");
+            System.out.println("결제 수단이 없습니다");
             result = false;
         }
+
         if (result) {
-            System.out.println("결제가 성공했습니다.");
+            System.out.println("결제가 성공했습니다");
         } else {
-            System.out.println("결제가 실패했습니다.");
+            System.out.println("결제가 실패했습니다");
         }
     }
 }
